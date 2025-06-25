bf52b96e36f6b6cac1fc9a9c1cbe5395aba81cd3
Justin Lee <justin.r.lee@gmail.com>
2025-06-25 00:30:14 +0000

    Use different principal for each service

--- deploy_oidc_demo.sh
+++ deploy_oidc_demo.sh
@@ -18,6 +18,26 @@ clientId=confluent-sso
 clientSecret=Wyt2jq5gzqrONotJ4W7R07OA4yUnnj3l
 EOF
 
+tee ${LOCAL_DIR}/kafka_oauth_jaas.txt <<-'EOF'
+clientId=kafka
+clientSecret=LpXqoU8bqCqXgNsPKAJIJhQ9WafgWwsj
+EOF
+
+tee ${LOCAL_DIR}/schemaregistry_oauth_jaas.txt <<-'EOF'
+clientId=schemaregistry
+clientSecret=1f3725004736c14da0299f25eeb7d853
+EOF
+
+tee ${LOCAL_DIR}/connect_oauth_jaas.txt <<-'EOF'
+clientId=connect
+clientSecret=0d717d281979a760daffe77b397830eb
+EOF
+
+tee ${LOCAL_DIR}/controlcenter_oauth_jaas.txt <<-'EOF'
+clientId=controlcenter
+clientSecret=7dfe814e68561764539f09ae49012ac8
+EOF
+
 kubectl create -n ${NAMESPACE} secret generic \
     oauth-jaas \
     --from-file=oauth.txt=${LOCAL_DIR}/oauth_jaas.txt \
@@ -33,6 +53,38 @@ kubectl create -n ${NAMESPACE} secret generic \
     --dry-run=client -oyaml --save-config \
     > ${LOCAL_DIR}/sso-oauth-jaas.yaml
 kubectl apply -f ${LOCAL_DIR}/sso-oauth-jaas.yaml
+
+kubectl create -n ${NAMESPACE} secret generic \
+    kafka-oauth-jaas \
+    --from-file=oauth.txt=${LOCAL_DIR}/kafka_oauth_jaas.txt \
+    --from-file=oidcClientSecret.txt=${LOCAL_DIR}/kafka_oauth_jaas.txt \
+    --dry-run=client -oyaml --save-config \
+    > ${LOCAL_DIR}/kafka-oauth-jaas.yaml
+kubectl apply -f ${LOCAL_DIR}/kafka-oauth-jaas.yaml
+
+kubectl create -n ${NAMESPACE} secret generic \
+    schemaregistry-oauth-jaas \
+    --from-file=oauth.txt=${LOCAL_DIR}/schemaregistry_oauth_jaas.txt \
+    --from-file=oidcClientSecret.txt=${LOCAL_DIR}/schemaregistry_oauth_jaas.txt \
+    --dry-run=client -oyaml --save-config \
+    > ${LOCAL_DIR}/schemaregistry-oauth-jaas.yaml
+kubectl apply -f ${LOCAL_DIR}/schemaregistry-oauth-jaas.yaml
+
+kubectl create -n ${NAMESPACE} secret generic \
+    connect-oauth-jaas \
+    --from-file=oauth.txt=${LOCAL_DIR}/connect_oauth_jaas.txt \
+    --from-file=oidcClientSecret.txt=${LOCAL_DIR}/connect_oauth_jaas.txt \
+    --dry-run=client -oyaml --save-config \
+    > ${LOCAL_DIR}/connect-oauth-jaas.yaml
+kubectl apply -f ${LOCAL_DIR}/connect-oauth-jaas.yaml
+
+kubectl create -n ${NAMESPACE} secret generic \
+    controlcenter-oauth-jaas \
+    --from-file=oauth.txt=${LOCAL_DIR}/controlcenter_oauth_jaas.txt \
+    --from-file=oidcClientSecret.txt=${LOCAL_DIR}/controlcenter_oauth_jaas.txt \
+    --dry-run=client -oyaml --save-config \
+    > ${LOCAL_DIR}/controlcenter-oauth-jaas.yaml
+kubectl apply -f ${LOCAL_DIR}/controlcenter-oauth-jaas.yaml
 
 deploy_manifests ${MANIFEST_DIR}
 
