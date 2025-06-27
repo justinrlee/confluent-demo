4df34beb855f2263d1761393cd091198600fdeeb
Justin Lee <jlee@confluent.io>
2025-06-27 16:13:06 +0800

    Add oidc stuff

---
+++ destroy_oidc_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./remove_oidc_demo.sh
+./remove_infra_keycloak.sh
+./remove_infra.sh
---
+++ run_oidc_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./deploy_infra.sh
+./deploy_infra_keycloak.sh
+./deploy_oidc_demo.sh
